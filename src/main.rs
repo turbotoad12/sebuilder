9383523290167e9141c9b222c420aa61d4648d8c
Turbotoad12 <jones_zachary@outlook.com>
2026-06-15 14:22:54 -0500

    Add platform support.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 pub mod cmake;
 pub mod git;
 pub mod utils;
+pub mod zip;
 
 use crate::cmake::update_cmake_set;
 use crate::git::clone_tag;
 use crate::utils::run_cross_platform;
 use seahorse::{App, Context, Flag, FlagType};
 use std::env;
+use crate::zip::unzip_file;
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
@@ -39,6 +41,11 @@ fn main() {
             Flag::new("sb3", FlagType::String)
                 .description("SB3 file to build")
                 .alias("s"),
+        )
+        .flag(
+            Flag::new("platform", FlagType::String)
+                .description("Produced platform (3ds, wiiu, etc.)")
+                .alias("p"),
         );
 
     app.run(args);
@@ -55,7 +62,17 @@ fn default_action(c: &Context) {
     let description = c.string_flag("description").unwrap();
     let author = c.string_flag("author").unwrap();
     let sb3_file = c.string_flag("sb3").unwrap();
+    // Check if the platform flag is set
+    let mut platform = c.string_flag("platform").unwrap_or_default();
+    if platform.is_empty() {
+        println!("Platform flag not set, defaulting to 3ds");
+        platform.push_str("3ds");
+    }
 
+    // Check if ./se exists, if so delete it
+    if std::path::Path::new("./se").exists() {
+        std::fs::remove_dir_all("./se").expect("Failed to remove existing SE directory");
+    }
     clone_tag(
         "https://github.com/ScratchEverywhere/ScratchEverywhere.git",
         None,
@@ -71,9 +88,11 @@ fn default_action(c: &Context) {
         .expect("Failed to update CMake set");
 
     std::fs::create_dir_all("./se/romfs/project").expect("Failed to create project directory");
-    std::fs::copy(&sb3_file, "./se/romfs/project/project.sb3").expect("Failed to copy SB3 file");
+    // unzip the sb3 file into the project directory
+    unzip_file(&sb3_file, "./se/romfs/project").expect("Failed to unzip SB3 file");
 
+    // Build the project
     run_cross_platform(
-        "docker build -f ./se/docker/Dockerfile.3ds --target exporter -o ./se ./se",
+        format!("docker build -f ./se/docker/Dockerfile.{} --target exporter -o ./se ./se", platform).as_str(),
     ).expect("Failed to run cross-platform command");
 }
